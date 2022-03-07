b1a9a16595002b58271573e30f1d948fdd32d27b
disuns <xyxo1992@naver.com>
2022-03-07 23:38:32 +0900

    간단한 테마 및 액티비티 틀잡기

---
+++ app/src/main/java/com/sjchoi/weather/fragment/TabFragment.kt
@@ -0,0 +1,21 @@
+package com.sjchoi.weather.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class TabFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+    }
+}
